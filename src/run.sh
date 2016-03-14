727c8749f853db05f1b8db05dd8dbf6cc1fcde03
george <goblin_george@gmail.com>
2016-03-14 09:45:40 +0800

    update

--- src/run.sh
+++ src/run.sh
@@ -7,7 +7,7 @@
 #
 
 set -e
-git clone https://$GITHUB_USER:$GITHUB_TOKEN@github.com/$GITHUB_REPO --branch=$REPO_BRANCH --depth=1 /work
+git clone https://$GITHUB_USER:$GITHUB_TOKEN@github.com/$GITHUB_REPO --branch=$GITHUB_REPO_BRANCH --depth=1 /work
 cd /work 
 git submodule init 
 git submodule update
